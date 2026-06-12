a6ea542cb8a4717f77fbe897f8df3e1a7a719f5f
seckfly <marcomartellc@gmail.com>
2026-06-11 20:12:05 -0500

    Implementar busqueda y actualizacion de estado de venta

--- Ventas.cs
+++ Ventas.cs
@@ -1,1 +1,13 @@
-
+namespace SoftwareGestorDeVentas
+{
+    internal class Venta
+    {
+        public string Codigo;
+        public string Cliente;
+        public string Producto;
+        public int Cantidad;
+        public double Precio;
+        public double Total;
+        public string Estado;
+    }
+}
