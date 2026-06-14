0df830f4cfaa66fbebec5e8e770845d6292a9e40
yeremy velasquez <yeremy.2006.vel@gmail.com>
2026-06-14 12:42:14 -0500

    Implementar módulo Carrito de Compras
    Se agregó la funcionalidad para visualizar los pedidos registrados y calcular el total general del carrito utilizando la información almacenada en RegistrarVenta

--- CarritoDeCompras.cs
+++ CarritoDeCompras.cs
@@ -1,14 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 
 namespace SoftwareGestorDeVentas
 {
-    internal class CarritoDeCompras
+    internal class Carrito
     {
         public static void MostrarCarrito()
         {
+            Console.WriteLine("\n===== CARRITO DE COMPRAS =====");
 
+            // Verificar si existen pedidos registrados
+            if (RegistrarVenta.contador_de_pedidos == 0)
+            {
+                Console.WriteLine("El carrito está vacío.");
+                return;
+            }
+
+            double totalGeneral = 0;
+
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("N°\tCliente\t\tProducto\tCant.\tPrecio\t\tTotal");
+            Console.WriteLine("--------------------------------------------------------------------------");
+
+            // Recorrer todos los pedidos registrados
+            for (int i = 0; i < RegistrarVenta.contador_de_pedidos; i++)
+            {
+                Console.WriteLine(
+                    (i + 1) + "\t" +
+                    RegistrarVenta.arreglo_de_clientes[i] + "\t\t" +
+                    RegistrarVenta.arreglo_de_productos[i] + "\t\t" +
+                    RegistrarVenta.arreglo_de_cantidades[i] + "\t" +
+                    "S/ " + RegistrarVenta.arreglo_de_precios[i].ToString("F2") + "\t" +
+                    "S/ " + RegistrarVenta.arreglo_de_totales[i].ToString("F2")
+                );
+
+                totalGeneral += RegistrarVenta.arreglo_de_totales[i];
+            }
+
+            Console.WriteLine("--------------------------------------------------------------------------");
+            Console.WriteLine("TOTAL DEL CARRITO: S/ " + totalGeneral.ToString("F2"));
+            Console.WriteLine("--------------------------------------------------------------------------");
         }
     }
 }
