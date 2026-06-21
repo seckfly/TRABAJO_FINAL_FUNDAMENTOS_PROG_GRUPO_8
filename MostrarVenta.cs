49444edb4844c8a8641ac618cc3e46d81b5f2679
seckfly <marcomartellc@gmail.com>
2026-06-21 01:23:45 -0500

    Corrige registro y visualizacion de productos por boleta

--- MostrarVenta.cs
+++ MostrarVenta.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SoftwareGestorDeVentas
 {
@@ -11,22 +9,45 @@ namespace SoftwareGestorDeVentas
             Console.Write("Ingrese el código de venta: ");
             string codigoBuscado = Console.ReadLine();
 
+            bool encontrado = false;
+            double totalBoleta = 0;
+            string cliente = "";
+
+            Console.WriteLine("\n***** VENTA *****");
+
             foreach (Venta venta in RegistrarVenta.ventas)
             {
                 if (venta.Codigo == codigoBuscado)
                 {
-                    Console.WriteLine("\n***** VENTA *****");
-                    Console.WriteLine("Código: " + venta.Codigo);
-                    Console.WriteLine("Cliente: " + venta.Cliente);
+                    if (!encontrado)
+                    {
+                        cliente = venta.Cliente;
+                        Console.WriteLine("Código: " + venta.Codigo);
+                        Console.WriteLine("Cliente: " + cliente);
+                        Console.WriteLine("--------------------------");
+                    }
+
+                    encontrado = true;
+
                     Console.WriteLine("Producto: " + venta.Producto);
                     Console.WriteLine("Cantidad: " + venta.Cantidad);
                     Console.WriteLine("Precio Unitario: " + venta.Precio);
-                    Console.WriteLine("Total: " + venta.Total);
-                    return;
+                    Console.WriteLine("Subtotal: " + venta.Total);
+                    Console.WriteLine("--------------------------");
+
+                    totalBoleta += venta.Total;
                 }
             }
 
-            Console.WriteLine("No se encontró una venta con ese código.");
+            if (encontrado)
+            {
+                Console.WriteLine("Total de la boleta: " + totalBoleta);
+                Console.WriteLine("*******************");
+            }
+            else
+            {
+                Console.WriteLine("No se encontró una venta con ese código.");
+            }
         }
     }
 }
