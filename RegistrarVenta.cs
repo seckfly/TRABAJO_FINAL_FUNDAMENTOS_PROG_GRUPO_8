ab21a586043f9ad99f3cf59be1fa87c996d29210
seckfly <marcomartellc@gmail.com>
2026-06-21 01:22:23 -0500

    Corrige registro de multiples productos en una misma boleta

--- RegistrarVenta.cs
+++ RegistrarVenta.cs
@@ -1,15 +1,13 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
 
 namespace SoftwareGestorDeVentas
 {
     internal class RegistrarVenta
     {
-
         static int contadorVentas = 1;
         public static List<Venta> ventas = new List<Venta>();
+
         public static void RegistroDeVenta()
         {
             string persona;
@@ -17,57 +15,49 @@ namespace SoftwareGestorDeVentas
             int cantidad = 0;
             double precioUnitario = 0;
             double total = 0;
+            double totalBoleta = 0;
             string opcion;
             string codigoVenta;
 
+            codigoVenta = "V" + contadorVentas.ToString("0000");
+            contadorVentas++;
+
+            Console.WriteLine("Ingresa tu nombre y apellidos");
+            persona = Console.ReadLine();
+
+            while (persona == "")
+            {
+                Console.WriteLine("Por favor, ingresa tu nombre y apellido");
+                persona = Console.ReadLine();
+            }
+
             do
             {
-                Console.WriteLine("Ingresa tu nombre y apellidos");
-                persona = Console.ReadLine();
-                do
-                {
-                    if (persona == "") 
-                    {
-                        Console.WriteLine("Por favor, ingresa tu nombre y apellido");
-                        persona = Console.ReadLine();
-                    }
-                } while (persona == "");
-
                 Console.WriteLine("Ingresa el nombre del producto: ");
                 producto = Console.ReadLine();
-                do
+
+                while (producto == "")
                 {
-                    if (producto == "")
-                    {
-                        Console.WriteLine("Porfavor, ingresa el nombre del producto: ");
-                        producto = Console.ReadLine();
-                    }
-                } while (producto == "");
+                    Console.WriteLine("Por favor, ingresa el nombre del producto: ");
+                    producto = Console.ReadLine();
+                }
 
                 Console.WriteLine("Ingresa la cantidad que deseas: ");
-                do
+                while (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
                 {
-                    if (!int.TryParse(Console.ReadLine(), out cantidad) || cantidad <= 0)
-                    {
-                        Console.WriteLine("Ingresa un número entero mayor a 0.");
-                    }
-                } while (cantidad <= 0);
+                    Console.WriteLine("Ingresa un número entero mayor a 0.");
+                }
 
                 Console.WriteLine("Ingresa su precio unitario");
-                do
+                while (!double.TryParse(Console.ReadLine(), out precioUnitario) || precioUnitario <= 0)
                 {
-                    if (!double.TryParse(Console.ReadLine(), out precioUnitario) || precioUnitario <= 0)
-                    {
-                        Console.WriteLine("Porfavor, el precio unitartio debe ser un numero entero y mayor a 0");
-                    }
-                } while (precioUnitario <= 0);
+                    Console.WriteLine("Por favor, el precio unitario debe ser un número mayor a 0");
+                }
 
                 total = precioUnitario * cantidad;
-                codigoVenta = "V" + contadorVentas.ToString("0000");
-                contadorVentas++;
+                totalBoleta += total;
 
                 Venta nuevaVenta = new Venta();
-
                 nuevaVenta.Codigo = codigoVenta;
                 nuevaVenta.Cliente = persona;
                 nuevaVenta.Producto = producto;
@@ -78,25 +68,26 @@ namespace SoftwareGestorDeVentas
 
                 ventas.Add(nuevaVenta);
 
-                Console.WriteLine("******Boleta******");
-                Console.WriteLine("*** N° " + codigoVenta);
-                Console.WriteLine("Cliente: " + persona);
-                Console.WriteLine("Producto:" + producto);
+                Console.WriteLine("Producto agregado a la boleta " + codigoVenta);
+                Console.WriteLine("Producto: " + producto);
                 Console.WriteLine("Cantidad: " + cantidad);
                 Console.WriteLine("Precio Unitario: " + precioUnitario);
-                Console.WriteLine("Total:" + total);
-                Console.WriteLine("******************");
-
+                Console.WriteLine("Subtotal: " + total);
 
                 do
                 {
-                    Console.WriteLine("¿Deseas registrar otro producto (SI/NO)");
+                    Console.WriteLine("¿Deseas registrar otro producto en la misma boleta? (SI/NO)");
                     opcion = Console.ReadLine().ToUpper();
-
                 } while (opcion != "SI" && opcion != "NO");
 
             } while (opcion == "SI");
-               
+
+            Console.WriteLine("****** Boleta ******");
+            Console.WriteLine("*** N° " + codigoVenta);
+            Console.WriteLine("Cliente: " + persona);
+            Console.WriteLine("Total de la boleta: " + totalBoleta);
+            Console.WriteLine("Estado: PENDIENTE");
+            Console.WriteLine("********************");
         }
     }
 }
